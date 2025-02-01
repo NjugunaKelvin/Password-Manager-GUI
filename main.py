219322b2f849db152fa92ae4fe336c6c0b4c073b
NjugunaKelvin <njugunak349@gmail.com>
2025-02-01 11:14:20 +0300

    Implement password saving functionality with validation and message prompts

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+
+# Password Generator
 
 
 
 
 
+# Save Password
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
 
+    if len(website)== 0 or len(password) == 0:
+        is_ok = False
+        messagebox.showinfo(title="Ooops", message="Seems like you left some field blank")
+    else:
+        # message box
+        is_ok = messagebox.askokcancel(title="Website", message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \n Is it okay to save")
 
+        
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
 
+                # clear
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 
@@ -49,7 +71,7 @@ website_entry.focus()
 
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(END)
+email_entry.insert(0, "johndoe@gmail.com")
 
 password_entry = Entry(width=17)
 password_entry.grid(row=3, column=1)
@@ -59,7 +81,7 @@ password_entry.grid(row=3, column=1)
 generate_pass_btn =Button(text="Generate Password")
 generate_pass_btn.grid(row=3, column=2)
 
-add_btn = Button(text="Add", width=36)
+add_btn = Button(text="Add", width=36, command=save)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 
